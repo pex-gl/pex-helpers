f985c099858668602f7d4dd52e5e9567efc6e076
Marcin Ignac <marcin.ignac@gmail.com>
2014-05-05 18:47:27 +0100

    Added AxisHelper

--- index.js
+++ index.js
@@ -3,3 +3,4 @@ module.exports.EdgeHelper = require('./lib/EdgeHelper');
 module.exports.FaceHelper = require('./lib/FaceHelper');
 module.exports.VertexNormalHelper = require('./lib/VertexNormalHelper');
 module.exports.FaceNormalHelper = require('./lib/FaceNormalHelper');
+module.exports.AxisHelper = require('./lib/AxisHelper');
